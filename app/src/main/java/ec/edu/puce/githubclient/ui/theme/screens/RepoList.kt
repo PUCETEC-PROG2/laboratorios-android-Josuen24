df9cfa2e2664edca34a7b984c37e4171823033ce
Josuen24 <jdnicolalde@puce.edu.ec>
2026-05-13 16:37:52 -0500

    primer commit 1

--- app/src/main/java/ec/edu/puce/githubclient/ui/theme/screens/RepoList.kt
+++ app/src/main/java/ec/edu/puce/githubclient/ui/theme/screens/RepoList.kt
@@ -1,26 +1,45 @@
-package ec.edu.puce.githubclient.ui.theme.screens
+package ec.edu.puce.githubclient.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import ec.edu.puce.githubclient.ui.theme.compose.RepoItem
 
+@Preview(showBackground = true)
 @Composable
-fun RepoList() {
-    Column {
+fun RepoList(
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier
+    ) {
+
         RepoItem(
-            name = "Repositorio de Android",
-            description = "Repositorio creado para pruebas",
+            name = "Repositorio",
+            description = "Repositorio creado para desarrollo móvil",
             avatarUrl = "https://avatars.githubusercontent.com/u/1?v=4",
             lenguaje = "Kotlin"
         )
+
         RepoItem(
-            name = "Repositorio de Android",
-            description = "Repositorio creado para pruebas",
+            name = "Repositorio ",
+            description = "Repositorio creado para desarrollo móvil",
             avatarUrl = "https://avatars.githubusercontent.com/u/1?v=4",
             lenguaje = "Kotlin"
         )
+
         RepoItem(
-            name = "Repositorio de Android",
-            description = "Repositorio creado para pruebas",
+            name = "Repositorio",
+            description = "Repositorio creado para desarrollo móvil",
+            avatarUrl = "https://avatars.githubusercontent.com/u/1?v=4",
+            lenguaje = "Kotlin"
+        )
+
+        RepoItem(
+            name = "Repositorio ",
+            description = "Repositorio creado para desarrollo móvil",
             avatarUrl = "https://avatars.githubusercontent.com/u/1?v=4",
             lenguaje = "Kotlin"
         )
