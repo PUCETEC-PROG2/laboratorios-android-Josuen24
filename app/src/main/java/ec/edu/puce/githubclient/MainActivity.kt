36840b6a5518ecc823312d53325810544d35401e
Josue Nicolalde <jdnicolalde@puce.edu.ec>
2026-05-27 16:37:57 -0500

    Commit miercoles 27

--- app/src/main/java/ec/edu/puce/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/puce/githubclient/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ec.edu.puce.githubclient.ui.screens.RepoList
 import ec.edu.puce.githubclient.ui.theme.GithubClientTheme
 
@@ -20,10 +17,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GithubClientTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    RepoList(modifier = Modifier.padding(innerPadding))
+                    RepoList()
                 }
             }
         }
     }
-}
